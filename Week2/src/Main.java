d659475b3b80fe55cc09255a3ea9d290c93e1bad
Reviara <ajgleeuw@avans.nl>
2013-09-02 16:39:52 +0200

    Week2 Testdata

--- Week2/src/Main.java
+++ Week2/src/Main.java
@@ -16,17 +16,17 @@ public class Main {
 
     }
 
-    8             // aantal punten
-            900,100,      // coordinaat 0
-            100,100,      // coordinaat 1
-            50,750,       // coordinaat 2
-            110,90,       // coordinaat 3
-            950,50,       // coordinaat 4
-            60,800,       // coordinaat 5
-            40,800,       // coordinaat 6
-            700,850,      // coordinaat 7
-    private void createTestData()
+    private static SpelObject[] createTestData()
     {
-
+        SpelObject[] array = new SpelObject[8];
+        array[0] = new SpelObject(900, 100);
+        array[1] = new SpelObject(100, 100);
+        array[2] = new SpelObject(50, 750);
+        array[3] = new SpelObject(110, 90);
+        array[4] = new SpelObject(950, 50);
+        array[5] = new SpelObject(60, 800);
+        array[6] = new SpelObject(40, 800);
+        array[7] = new SpelObject(700, 850);
+        return array;
     }
 }
--- Week2/src/SpelObject.java
+++ Week2/src/SpelObject.java
@@ -10,6 +10,12 @@ public class SpelObject
     public static final int DIMENSION = 2;
     private double[] position = new double[DIMENSION];
 
+    public SpelObject(double x, double y)
+    {
+        position[0] = y;
+        position[1] = x;
+    }
+
     // 0 = x, y = 1
     public double getPosition(int index )
     {

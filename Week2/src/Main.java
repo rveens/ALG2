d087919e2e17af61e85dc3c324407429532e4f26
Reviara <ajgleeuw@avans.nl>
2013-09-02 15:49:51 +0200

    Week2 initial

---
+++ Week2/src/Main.java
@@ -0,0 +1,18 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Reviara
+ * Date: 2-9-13
+ * Time: 15:35
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+    public Main()
+    {
+
+    }
+
+    public static void main(String[] args)
+    {
+
+    }
+}

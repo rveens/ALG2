eb5c680b8af70b1e3b8e601375e432ed8cce4e4f
Reviara <ajgleeuw@avans.nl>
2013-10-21 12:41:23 +0200

    Eerste commit week 6

---
+++ week6/src/HashTabel.java
@@ -0,0 +1,11 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Reviara
+ * Date: 21-10-13
+ * Time: 11:43
+ * To change this template use File | Settings | File Templates.
+ */
+public class HashTabel {
+
+
+}
